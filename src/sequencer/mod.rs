3ca85427d4fdd52eafbf9b9001db5c100c06ed3a
epellis <nedellis1234@gmail.com>
2022-03-18 15:18:21 -0700

    Split sequencer into server and implementation

--- src/sequencer/mod.rs
+++ src/sequencer/mod.rs
@@ -1,43 +1,88 @@
 use crate::calvinite_tonic::sequencer_grpc_service_server::SequencerGrpcService;
 use crate::calvinite_tonic::{RunStmtRequest, RunStmtRequestWithUuid, RunStmtResponse};
+use std::collections::HashMap;
 use std::fmt::Debug;
+use std::sync::{Arc, Mutex};
+use tokio::sync;
+use tokio::sync::broadcast::{Receiver, Sender};
 
 use crate::scheduler::Scheduler;
 
 use tonic::Response;
 use uuid::Uuid;
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct Sequencer {
     scheduler: Scheduler,
+    global_req_log_rx: Receiver<RunStmtRequestWithUuid>,
+    finished_txn_notifier: Arc<Mutex<HashMap<Uuid, sync::oneshot::Sender<RunStmtResponse>>>>,
 }
 
 impl Sequencer {
-    pub fn new(scheduler: Scheduler) -> Self {
-        Self { scheduler }
+    pub fn new(
+        scheduler: Scheduler,
+        global_req_log_rx: Receiver<RunStmtRequestWithUuid>,
+        finished_txn_notifier: Arc<Mutex<HashMap<Uuid, sync::oneshot::Sender<RunStmtResponse>>>>,
+    ) -> Self {
+        Self {
+            scheduler,
+            global_req_log_rx,
+            finished_txn_notifier,
+        }
+    }
+
+    pub async fn serve(&mut self) {
+        loop {
+            let req = self.global_req_log_rx.recv().await.unwrap();
+
+            let uuid = Uuid::parse_str(&req.uuid).unwrap();
+
+            let res = self.scheduler.submit_txn(req).await.unwrap();
+
+            // If SequencerServer is local, notify that the txn is complete
+            {
+                let mut finished_txn_notifier = self.finished_txn_notifier.lock().unwrap();
+                if let Some(tx) = finished_txn_notifier.remove(&uuid) {
+                    tx.send(res);
+                }
+            }
+        }
     }
 }
 
+#[derive(Debug)]
+pub struct SequencerServer {
+    global_req_log_tx: Sender<RunStmtRequestWithUuid>,
+    finished_txn_notifier: Arc<Mutex<HashMap<Uuid, sync::oneshot::Sender<RunStmtResponse>>>>,
+}
+
 #[tonic::async_trait]
-impl SequencerGrpcService for Sequencer {
+impl SequencerGrpcService for SequencerServer {
     async fn run_stmt(
         &self,
         request: tonic::Request<RunStmtRequest>,
     ) -> Result<tonic::Response<RunStmtResponse>, tonic::Status> {
         let run_stmt_request = request.into_inner();
 
-        let txn_uuid = Uuid::new_v4().to_string();
+        let txn_uuid = Uuid::new_v4();
 
-        let completed_txn = self
-            .scheduler
-            .submit_txn(RunStmtRequestWithUuid {
-                query: run_stmt_request.query.clone(),
-                uuid: txn_uuid.clone(),
-            })
-            .await
-            .unwrap();
+        let req = RunStmtRequestWithUuid {
+            query: run_stmt_request.query.clone(),
+            uuid: txn_uuid.to_string().clone(),
+        };
 
-        Ok(Response::new(completed_txn))
+        let (finished_txn_tx, finished_txn_rx) = sync::oneshot::channel();
+
+        {
+            let mut finished_txn_notifier = self.finished_txn_notifier.lock().unwrap();
+            finished_txn_notifier.insert(txn_uuid, finished_txn_tx);
+        }
+
+        self.global_req_log_tx.send(req);
+
+        let res = finished_txn_rx.await.unwrap();
+
+        Ok(Response::new(res))
     }
 }
 
