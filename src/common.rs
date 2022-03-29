6a2432ab689dd249175ad8edc7437df200f8245e
epellis <nedellis1234@gmail.com>
2022-03-28 21:43:00 -0700

    Greatly simplify sharding

--- src/common.rs
+++ src/common.rs
@@ -2,8 +2,8 @@ use std::mem;
 
 use serde::{Deserialize, Serialize};
 
-pub type VirtualNodeSize = u16;
-pub const VIRTUAL_NODE_SIZE_BITS: usize = mem::size_of::<VirtualNodeSize>();
+pub type VirtualNodeType = u16;
+pub const VIRTUAL_NODE_SIZE_BITS: usize = mem::size_of::<VirtualNodeType>();
 
 // TODO: Maybe just make the virtual node
 #[derive(Debug, Clone, Hash, PartialEq, PartialOrd, Eq, Serialize, Deserialize)]
@@ -12,14 +12,21 @@ pub struct Record {
 }
 
 impl Record {
-    fn virtual_node(&self) -> [u8; VIRTUAL_NODE_SIZE_BITS] {
+    fn virtual_node_byte_arry(&self) -> [u8; VIRTUAL_NODE_SIZE_BITS] {
         let bytes = bincode::serialize(self).unwrap();
         let digest: [u8; 16] = md5::compute(bytes).into();
         digest[..VIRTUAL_NODE_SIZE_BITS].try_into().unwrap()
+        // VirtualNodeSize::from_be_bytes(digest);
+    }
+
+    pub fn virtual_node(&self) -> VirtualNodeType {
+        let bytes = bincode::serialize(self).unwrap();
+        let digest: [u8; 16] = md5::compute(bytes).into();
+        VirtualNodeType::from_le_bytes(digest[..VIRTUAL_NODE_SIZE_BITS].try_into().unwrap())
     }
 
     pub fn fully_qualified_id_as_bytes(&self) -> Vec<u8> {
-        let virtual_node = self.virtual_node();
+        let virtual_node = self.virtual_node_byte_arry();
         let bytes = bincode::serialize(self).unwrap();
         [virtual_node.to_vec(), bytes].concat()
     }
--- src/executor/peer.rs
+++ src/executor/peer.rs
@@ -1,184 +1,31 @@
 use std::{collections::HashMap, ops::Range};
 
-use crate::common::VirtualNodeSize;
+use crate::common::{Record, VirtualNodeType};
 use std::num::Wrapping;
 use uuid::Uuid;
 
+#[derive(Debug, Clone, Copy, Hash, PartialEq, PartialOrd, Eq)]
+pub struct Peer {
+    pub id: Uuid,
+}
+
 #[derive(Debug, Clone)]
 pub struct PeerManager {
+    pub me: Peer,
     pub local_peers: Vec<Peer>, // Dev only!, also this includes yourself
 }
 
 impl PeerManager {
-    pub fn get_local_peer_ranges(&self) -> HashMap<Peer, Range<Wrapping<VirtualNodeSize>>> {
+    pub fn get_ordered_peers(&self) -> Vec<Peer> {
         let mut sorted_peers = self.local_peers.clone();
         sorted_peers.sort_by_key(|p| p.id.as_u128());
-
-        let mut start = Wrapping::<VirtualNodeSize>::MIN;
-        let step_size = Wrapping::<VirtualNodeSize>(
-            VirtualNodeSize::MAX / self.local_peers.len() as VirtualNodeSize,
-        );
-
-        let mut peer_to_range = HashMap::default();
-
-        let (last_peer, other_peers) = sorted_peers.split_last().unwrap();
-        for peer in other_peers.iter().cloned() {
-            peer_to_range.insert(
-                peer,
-                Range {
-                    start: start,
-                    end: start + step_size,
-                },
-            );
-            start += step_size;
-        }
-
-        peer_to_range.insert(
-            last_peer.clone(),
-            Range {
-                start: start,
-                end: Wrapping::<VirtualNodeSize>::MIN,
-            },
-        );
-
-        peer_to_range
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use std::{collections::HashMap, num::Wrapping, ops::Range};
-
-    use uuid::Uuid;
-
-    use crate::common::VirtualNodeSize;
-
-    use super::{Peer, PeerManager};
-
-    #[test]
-    fn get_local_peer_ranges_1_peer() {
-        let pm = PeerManager {
-            local_peers: vec![Peer {
-                id: Uuid::from_u128(0),
-            }],
-        };
-
-        let peer_to_range = pm.get_local_peer_ranges();
-        assert_eq!(
-            peer_to_range,
-            [(
-                Peer {
-                    id: Uuid::from_u128(0)
-                },
-                Range {
-                    start: Wrapping::<VirtualNodeSize>(0),
-                    end: Wrapping::<VirtualNodeSize>(0)
-                }
-            )]
-            .iter()
-            .cloned()
-            .collect()
-        )
+        sorted_peers
     }
 
-    #[test]
-    fn get_local_peer_ranges_2_peers() {
-        let pm = PeerManager {
-            local_peers: vec![
-                Peer {
-                    id: Uuid::from_u128(0),
-                },
-                Peer {
-                    id: Uuid::from_u128(1),
-                },
-            ],
-        };
-
-        let peer_to_range = pm.get_local_peer_ranges();
-        assert_eq!(
-            peer_to_range,
-            [
-                (
-                    Peer {
-                        id: Uuid::from_u128(0)
-                    },
-                    Range {
-                        start: Wrapping::<VirtualNodeSize>(0),
-                        end: Wrapping::<VirtualNodeSize>(32767)
-                    }
-                ),
-                (
-                    Peer {
-                        id: Uuid::from_u128(1)
-                    },
-                    Range {
-                        start: Wrapping::<VirtualNodeSize>(32767),
-                        end: Wrapping::<VirtualNodeSize>(0)
-                    }
-                )
-            ]
-            .iter()
-            .cloned()
-            .collect()
-        )
+    pub fn peer_for_record(&self, record: &Record) -> Peer {
+        let ordered_peers = self.get_ordered_peers();
+        let virtual_node = record.virtual_node();
+        let idx = virtual_node / ordered_peers.len() as VirtualNodeType;
+        ordered_peers[idx as usize]
     }
-
-    #[test]
-    fn get_local_peer_ranges_3_peers() {
-        let pm = PeerManager {
-            local_peers: vec![
-                Peer {
-                    id: Uuid::from_u128(0),
-                },
-                Peer {
-                    id: Uuid::from_u128(1),
-                },
-                Peer {
-                    id: Uuid::from_u128(2),
-                },
-            ],
-        };
-
-        let peer_to_range = pm.get_local_peer_ranges();
-        assert_eq!(
-            peer_to_range,
-            [
-                (
-                    Peer {
-                        id: Uuid::from_u128(0)
-                    },
-                    Range {
-                        start: Wrapping::<VirtualNodeSize>(0),
-                        end: Wrapping::<VirtualNodeSize>(21845)
-                    }
-                ),
-                (
-                    Peer {
-                        id: Uuid::from_u128(1)
-                    },
-                    Range {
-                        start: Wrapping::<VirtualNodeSize>(21845),
-                        end: Wrapping::<VirtualNodeSize>(43690)
-                    }
-                ),
-                (
-                    Peer {
-                        id: Uuid::from_u128(2)
-                    },
-                    Range {
-                        start: Wrapping::<VirtualNodeSize>(43690),
-                        end: Wrapping::<VirtualNodeSize>(0)
-                    }
-                )
-            ]
-            .iter()
-            .cloned()
-            .collect()
-        )
-    }
-}
-
-#[derive(Debug, Clone, Hash, PartialEq, PartialOrd, Eq)]
-pub struct Peer {
-    pub id: Uuid,
 }
